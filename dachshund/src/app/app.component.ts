2e6e87bc57b255b138be613e8934dc02096fa053
Luisa Mora <luisamora895@gmail.com>
2025-02-06 11:38:03 -0400

    Estructura de lovers

--- dachshund/src/app/app.component.ts
+++ dachshund/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { LoversComponent } from "./pages/lovers/lovers.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,RouterLink],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
